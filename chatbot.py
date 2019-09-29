b08d501f075680fdd7d559c898a02482778e0baa
Erica Roque <erica.roque@gmail.com>
2019-09-29 20:47:02 -0300

    criação do chatbot com dataset em ingles

---
+++ chatbot.py
@@ -0,0 +1,56 @@
+# Construção do chatbot com Deep NLP#
+
+import numpy as np
+import tensorflow as tf
+import re # biblioteca para fazer transformações no texto
+import time
+
+
+# ---- Parte 1 - Pre-processamento dos dados #
+
+#Importação das bases de dados#
+linhas = open('movie-lines.txt', encoding = 'utf-8', errors= 'ignore').read().split('\n')
+conversas = open('movie-conversations.txt', encoding = 'utf-8', errors= 'ignore').read().split('\n')
+
+
+# Criação de um dicionário para mapear cada lina com seu ID#
+
+id_para_linha = {}
+for linha in linhas:
+    _linha = linha.split(' +++$+++ ')
+    #print(_linha)
+    if len(_linha) == 5:
+        #print(_linha)
+        id_para_linha[_linha[0]]= _linha[4]
+        
+# Criação de uma lista com todas as conversas
+conversas_id = []
+for conversa in conversas[:-1]:
+    #print(conversa)
+    _conversa= conversa.split(' +++$+++ ')[3][1:-1].replace("'","").replace(" ","")
+    #print(_conversa)
+    conversas_id.append(_conversa.split(','))
+   
+    
+    
+# Separação das perguntas e respostas
+perguntas=[]
+respostas=[]
+for conversa in conversas_id:
+    #print(conversa)
+    #print('*******')
+    for i in range(len(conversa)-1):
+        #print (i)
+        perguntas.append(id_para_linha[conversa[i]])
+        respostas.append(id_para_linha[conversa[i + 1]])
+
+# Pre processamento dos textos - melhorar a qualidade dos dados
+        
+def limpa_texto(texto):
+    texto = texto.lower()
+    texto = re.sub(r"i'm","i am", texto)
+    texto = re.sub(r"he's","he is", texto)
+    texto = re.sub(r"she's","she is", texto)
+    return texto
+
+limpa_texto=        
